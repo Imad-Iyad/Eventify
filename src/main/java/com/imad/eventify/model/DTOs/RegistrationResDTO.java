2e66199e8d026512cc1dfbb00c891784ee3a43d7
Imad Iyad <emadeyad063@gmail.com>
2025-09-02 13:51:27 +0300

    Configured prod profile for Render + Supabase

--- src/main/java/com/imad/eventify/model/DTOs/RegistrationResDTO.java
+++ src/main/java/com/imad/eventify/model/DTOs/RegistrationResDTO.java
@@ -15,8 +15,8 @@ public class RegistrationResDTO {
     private Long id;               // للتسجيل النهائي (بعد الحفظ)
     private Long userId;           // ID المستخدم
     private Long eventId;          // ID الحدث
-    private Long invitationId;     // ID الدعوة (اختياري، إذا جاي من دعوة)
-    private String inviteeEmail;   // البريد الإلكتروني من الدعوة
-    private String registrationToken; // Token للتسجيل النهائي (للتحقق / QR code)
-    private LocalDateTime registeredAt; // وقت التسجيل
+    private Long invitationId;
+    private String registrationToken;
+    private LocalDateTime registeredAt;
+
 }
